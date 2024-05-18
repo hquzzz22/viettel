8d23a3e003620a6efdd003165de53b3cf10b310d
hquzzz22 <hoang quyk23hubt@gmail.com>
2024-05-18 13:51:29 +0700

    conclick

--- main.js
+++ main.js
@@ -2,4 +2,16 @@ let click = document.querySelector(".action");
 
 const myFunction = () => {
   alert("Tinh nang chua hoan thien");
+
+  for (let i = 1; i <= 100; i++) {
+    console.log(i + ". Bạn Em Ngủ Thật Ngon Nhé <3");
+  }
 };
+
+let viettel_ca = document.querySelector("#viettel-ca");
+
+let signature = document.querySelector(".signature");
+function handleClick() {
+  signature.classList.toggle("hiden");
+}
+console.log(viettel_ca);
